db2f60adebfcddbeb6b2c40abef5be470dc70d29
Larry Mwansa <larrypmwansa@gmail.com>
2025-09-01 10:50:07 +0200

    Contact Page Html added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
-import styles from './contact.module.css';
+import React from "react";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
-  )
-}
+    <div>
+      <header>
+        <div>
+          <h3>Ready to Collaborate?</h3>
+          <h1>Ready to Collaborate?</h1>
+          <p>
+            Whether it’s a digital project, community initiative, or
+            collaboration opportunity, I’d love to hear from you.
+          </p>
+        </div>
+      </header>
+      <section>
+        <h2>Contact Options</h2>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <a href="mailto:">Email</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+              <li></li>
+            </ul>
+          </div>
+          <div>
+            <form>
+              <input type="text" name="name" placeholder="Your Name" required />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                required
+              />
+              <textarea
+                name="message"
+                placeholder="Your Message"
+                required
+              ></textarea>
+              <button type="submit" class="btn-primary">
+                Send A Message
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default ContactPage
+export default ContactPage;
